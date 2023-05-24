7473fb97b121935dae8ed4671f14743d780062f7
PrateekKhindri <prateek.khindri90@gmail.com>
2023-05-24 20:32:22 +1000

    Fetched, displayed 20 random users and added styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { CustomCard } from "./components/contacts/CustomCard";
+import { fetchContacts } from "./helpers/axiosHelper";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetchedContacts();
+  }, []);
+
+  const fetchedContacts = async () => {
+    const { data } = await fetchContacts();
+    setContacts(data.results);
+  };
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <CustomCard contacts={contacts} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
